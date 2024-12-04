91f2ab09d8a039c3d1479f571ee1a54f9eaed612
Nikos Zarifis <nikos.zarifis@gmail.com>
2024-12-04 17:05:16 +0200

    changes to procedures

--- db_scripts/procedures.sql
+++ db_scripts/procedures.sql
@@ -2,7 +2,7 @@ DROP PROCEDURE IF EXISTS log_user_login;
 DROP PROCEDURE IF EXISTS log_user_logout;
 
 DELIMITER $$
-CREATE PROCEDURE log_user_login(IN user_id INT)
+CREATE PROCEDURE log_user_login(IN Username VARCHAR(32))
 BEGIN
     INSERT INTO Audit_Logs (UserID, Type, Timestamp) VALUES (user_id, 'login', CURRENT_TIMESTAMP);
 END$$
@@ -11,7 +11,7 @@ DELIMITER ;
 
 
 DELIMITER $$
-CREATE PROCEDURE log_user_logout(IN user_id INT)
+CREATE PROCEDURE log_user_logout(IN Username VARCHAR(32))
 BEGIN
     INSERT INTO Audit_Logs (UserID, Type, Timestamp) VALUES (user_id, 'logout', CURRENT_TIMESTAMP);
 END$$
